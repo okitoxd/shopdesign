d6b04874e11415f1e6ab2fe3b9b899fba2b93598
Eduardo <okitoxd@gmail.com>
2016-11-08 22:12:58 -0500

    No me acuerdo que cambios le hice

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 $('nav').click(function () {
 	$(this).toggleClass('nav-show')
 })
+
+var modal = document.getElementById('modal')
+var modalImg = document.getElementById('modal__content')
+var modalCaption = document.getElementById('modal__caption')
+
+function openModal(img) {
+	modal.style.display = "flex"
+	modalImg.src = img;
+}
